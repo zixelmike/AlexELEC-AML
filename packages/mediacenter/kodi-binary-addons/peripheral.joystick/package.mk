f1f81ee623e73a99fe2a9ff8f6c4d284e1067fd8
Alexandr <alex@alexelec.in.ua>
2017-02-02 13:30:21 +0200

    kodi-binary-addons: upd peripheral.joystick

--- packages/mediacenter/kodi-binary-addons/peripheral.joystick/package.mk
+++ packages/mediacenter/kodi-binary-addons/peripheral.joystick/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="peripheral.joystick"
-PKG_VERSION="987a818"
+PKG_VERSION="c786dbf"
 PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
