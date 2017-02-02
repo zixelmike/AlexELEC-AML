c0dcd375ae7515caf768f94305b8759cbd134358
Alexandr <alex@alexelec.in.ua>
2017-02-02 12:29:09 +0200

    retroarch: update retroarch-1.4.0 and depends

--- packages/xmedia/games/retroarch-assets/package.mk
+++ packages/xmedia/games/retroarch-assets/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-assets"
-PKG_VERSION="41b3b3c"
+PKG_VERSION="6b434d5"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
