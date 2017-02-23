4bf9bb5116e962c96a84e39d0cece828679222db
Alexandr <alex@alexelec.in.ua>
2017-02-23 13:08:55 +0200

    kodi-17.1-rc1-82eaae9

--- packages/mediacenter/kodi-theme-Estuary/package.mk
+++ packages/mediacenter/kodi-theme-Estuary/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="kodi-theme-Estuary"
-GIT_VERSION="d2417c7"
+GIT_VERSION="82eaae9"
 PKG_VERSION="17.1-rc1-$GIT_VERSION"
 PKG_REV="1"
 PKG_ARCH="any"
