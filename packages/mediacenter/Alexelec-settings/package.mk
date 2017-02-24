46eb2b8d0f8f7c4f34baca839faceeaa4c981a69
Alexandr <alex@alexelec.in.ua>
2017-02-24 19:10:12 +0200

    acestream: add mount to SD-card for cache

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="9b080bd"
+PKG_VERSION="933e088"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
