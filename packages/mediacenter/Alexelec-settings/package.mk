72aa5673857d9bf668b816e534ad6d3ec4c124ba
Alexandr <alex@alexelec.in.ua>
2017-02-24 22:23:29 +0200

    acestream-aml: set umount in stop service

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="31ec695"
+PKG_VERSION="dcd56d1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
