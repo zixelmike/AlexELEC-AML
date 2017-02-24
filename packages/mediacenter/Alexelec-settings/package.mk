227db070554ca139ec2e0b557685f4df7978b52e
Alexandr <alex@alexelec.in.ua>
2017-02-24 19:28:57 +0200

    fix Alexelec.settings-aml

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="933e088"
+PKG_VERSION="31ec695"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
