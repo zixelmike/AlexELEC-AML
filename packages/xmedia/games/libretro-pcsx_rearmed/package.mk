5289733853ccd629d4be2dae586961fa3de1e6d8
Alexandr <alex@alexelec.in.ua>
2017-03-03 10:04:45 +0200

    libretro-pcsx_rearmed: bump to 9096ba8

--- packages/xmedia/games/libretro-pcsx_rearmed/package.mk
+++ packages/xmedia/games/libretro-pcsx_rearmed/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="libretro-pcsx_rearmed"
-PKG_VERSION="751c8c2"
+PKG_VERSION="9096ba8"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="GPLv2"
